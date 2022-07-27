bdd44a6e7ac2dee59a31d1a0d1daec85a8f8a65c
Diana <divnvp@yandex.com>
2022-07-27 20:29:30 +0300

    update: HomeComponent.vue

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-link to="/first">1</router-link>
+    <router-link to="/second">2</router-link>
+    <router-link to="/third">3</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "HomeComponent",
+});
+</script>
