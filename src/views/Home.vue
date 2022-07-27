fd1162994f17897a63cf6d8a2c5d6d856ca74515
Diana <divnvp@yandex.com>
2022-07-27 20:29:45 +0300

    update: Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <header>
+    <HomeComponent />
+  </header>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HomeComponent from "@/components/HomeComponent.vue";
 
 export default Vue.extend({
   name: "Home",
   components: {
-    HelloWorld,
+    HomeComponent,
   },
 });
 </script>
