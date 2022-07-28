4c2e549c5bd81ae347df36f380885f7ebd0e8f65
Diana <divnvp@yandex.com>
2022-07-28 13:18:37 +0300

    add: store content to store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,74 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { StoreOptions } from "vuex";
+
+import { Album, Post, RootState, User } from "@/types";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+const store: StoreOptions<RootState> = {
+  state: {
+    version: "1.0.0",
+    baseUrl: "https://jsonplaceholder.typicode.com",
+
+    users: [],
+    posts: [],
+    albums: [],
+  },
+
+  getters: {
+    findUser: (state: RootState) => (userId: number) => {
+      return state.users.find((user: User) => user.id === userId);
+    },
+  },
+
+  mutations: {
+    getUsers(state, payload: User[]): void {
+      state.users = payload;
+    },
+
+    getPosts(state, payload: Post[]): void {
+      state.posts = payload;
+    },
+
+    getAlbums(state, payload: Album[]): void {
+      state.albums = payload;
+    },
+  },
+
+  actions: {
+    getUsers({ commit, state }): void {
+      fetch(state.baseUrl + "/users").then(async (response) => {
+        if (response.ok) {
+          const payload: User = await response.json();
+          commit("getUsers", payload);
+        } else {
+          throw new Error(response.statusText);
+        }
+      });
+    },
+
+    getPosts({ commit, state }): void {
+      fetch(state.baseUrl + "/posts").then(async (response) => {
+        if (response.ok) {
+          const payload: Post = await response.json();
+          commit("getPosts", payload);
+        } else {
+          throw new Error(response.statusText);
+        }
+      });
+    },
+
+    getAlbums({ commit, state }): void {
+      fetch(state.baseUrl + "/albums").then(async (response) => {
+        if (response.ok) {
+          const payload: Post = await response.json();
+          commit("getAlbums", payload);
+        } else {
+          throw new Error(response.statusText);
+        }
+      });
+    },
+  },
+};
+
+export default new Vuex.Store<RootState>(store);
