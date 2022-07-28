5552949f5dc2c9caa51dfbc90bc99f5aab42ebff
Diana <divnvp@yandex.com>
2022-07-28 17:52:14 +0300

    add: album and post card

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <CardComponent :id="album.id" :name="user">
+    <template #content>
+      <span>TITLE: {{ album.title }}</span>
+    </template>
+  </CardComponent>
+</template>
+
+<script lang="ts">
+import { PropType } from "vue";
+import { Album } from "@/types";
+import store from "@/store";
+//Component
+import CardComponent from "@/components/CardComponent.vue";
+export default {
+  name: "AlbumCard",
+
+  components: { CardComponent },
+
+  props: {
+    album: {
+      type: Object as PropType<Album>,
+      required: true,
+    },
+  },
+
+  computed: {
+    user(): string {
+      return `User ID: ${this.album.userId} (${
+        store.getters.findUser(this.album.userId).name
+      })`;
+    },
+  },
+};
+</script>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <CardComponent :id="post.id" :name="user">
+    <template #content>
+      <span>TITLE: {{ post.title }}</span>
+      <span>BODY: {{ post.body }}</span>
+    </template>
+  </CardComponent>
+</template>
+
+<script lang="ts">
+import { PropType } from "vue";
+import { Post } from "@/types";
+import store from "@/store";
+//Component
+import CardComponent from "@/components/CardComponent.vue";
+
+export default {
+  name: "PostCard",
+
+  components: { CardComponent },
+
+  props: {
+    post: { type: Object as PropType<Post>, required: true },
+  },
+
+  computed: {
+    user(): string {
+      return `User ID: ${this.post.userId} (${
+        store.getters.findUser(this.post.userId).name
+      })`;
+    },
+  },
+};
+</script>
