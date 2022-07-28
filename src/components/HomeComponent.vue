9f60ed27da3fd89afb57f69b698b6ebc9eebb9a0
Diana <divnvp@yandex.com>
2022-07-28 12:05:25 +0300

    update: styles of HomeComponent.vue

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <router-link to="/first">1</router-link>
-    <router-link to="/second">2</router-link>
+  <div style="font-size: 18px">
+    <router-link to="/first" style="margin-right: 10px"> 1 </router-link>
+    <router-link to="/second" style="margin-right: 10px">2</router-link>
     <router-link to="/third">3</router-link>
   </div>
 </template>
