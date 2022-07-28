f9ff8240ac22478f7b054a942524b06b0e5a5ec2
Diana <divnvp@yandex.com>
2022-07-28 13:07:18 +0300

    add: new types to types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,51 @@
-export interface State {
-  posts: Array<post>;
-}
+export type RootState = {
+  version: string;
+  baseUrl: string;
 
-type post = {
+  users: Array<User>;
+  posts: Array<Post>;
+  albums: Array<Album>;
+};
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+};
+
+export type Post = {
   userId: number;
   id: number;
   title: string;
   body: string;
+};
+
+export type Album = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
+export type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+};
+
+type Geo = {
+  lat: string;
+  lng: string;
+};
+
+export type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 };
