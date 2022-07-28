775118afa4b4e5b18642e8917c18a6a2a1d83613
Diana <divnvp@yandex.com>
2022-07-28 13:17:45 +0300

    add: table of data in ThirdPage.vue

--- src/views/ThirdPage.vue
+++ src/views/ThirdPage.vue
@@ -1,11 +1,46 @@
 <template>
   <div>
     <h1>3</h1>
+    <button @click="$router.push('/')">Назад</button>
+
+    <table>
+      <thead>
+        <tr>
+          <td>User ID</td>
+          <td>ID</td>
+          <td>title</td>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="album in $store.state.albums" :key="album.id">
+          <td>
+            {{ album.userId }} ({{
+              $store.getters.findUser(album.userId).name
+            }})
+          </td>
+          <td>{{ album.id }}</td>
+          <td>{{ album.title }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
+import { mapGetters } from "vuex";
+import store from "@/store";
+
 export default {
   name: "SecondPage",
+
+  computed: {
+    ...mapGetters(["findUser"]),
+  },
+
+  mounted() {
+    store.dispatch("getUsers");
+    store.dispatch("getAlbums");
+  },
 };
 </script>
